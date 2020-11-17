cd21f89126a38a7729566f5628934b6b61c3034b
Galius <galius@posteo.de>
2020-11-17 23:38:27 +0100

    Fix indentation; stick to spaces for now.

--- background/background.js
+++ background/background.js
@@ -1,45 +1,45 @@
 function sendToForeground (message) {
-	browser.tabs.query({
-		active: true,
-		currentWindow: true
-	}, function sendToTab (tabs) {
-		browser.tabs.sendMessage(tabs[0].id, message);
-	});
+  browser.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function sendToTab (tabs) {
+    browser.tabs.sendMessage(tabs[0].id, message);
+  });
 }
 
 /** Crafted to retain focus. */
 class TranscriptRequestSystem {
-	constructor () {
-		TranscriptRequestSystem.startListening();
-	}
+  constructor () {
+    TranscriptRequestSystem.startListening();
+  }
 
-	static startListening () {
-		browser.webRequest.onBeforeRequest.addListener(
-			this.handleRequest, {
-				urls: [
-					"https://*.api.microsoftstream.com/api/videos/*/texttracks",
-					"https://*.api.microsoftstream.com/api/videos/*/texttracks?*"
-				]
-			}
-		);
-	}
+  static startListening () {
+    browser.webRequest.onBeforeRequest.addListener(
+      this.handleRequest, {
+        urls: [
+          "https://*.api.microsoftstream.com/api/videos/*/texttracks",
+          "https://*.api.microsoftstream.com/api/videos/*/texttracks?*"
+        ]
+      }
+    );
+  }
 
-	static stopListening() {
-		browser.webRequest.onBeforeRequest.removeListener(this.handleRequest);
-	}
+  static stopListening() {
+    browser.webRequest.onBeforeRequest.removeListener(this.handleRequest);
+  }
 
-	/** Gets lost easily. */
-	static handleRequest (requestDetails) {
-		TranscriptRequestSystem.stopListening();
+  /** Gets lost easily. */
+  static handleRequest (requestDetails) {
+    TranscriptRequestSystem.stopListening();
 
-		const transcriptDetails = requestDetails.url;
+    const transcriptDetails = requestDetails.url;
 
-		fetch(transcriptDetails)
-			.then(response => response.json())
-			.then(data => data.value[0].url)
-			.then(transcriptLocation => sendToForeground(transcriptLocation))
-			.then(() => TranscriptRequestSystem.startListening());
-	}
+    fetch(transcriptDetails)
+      .then(response => response.json())
+      .then(data => data.value[0].url)
+      .then(transcriptLocation => sendToForeground(transcriptLocation))
+      .then(() => TranscriptRequestSystem.startListening());
+  }
 }
 
 // And so it begins ...
